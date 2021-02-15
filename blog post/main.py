e070ce635a10e670a80354312bede24aaf20b8c0
kamena1994 <benjii1964@gmail.com>
2021-02-15 05:59:12 +0200

    blog update

--- blog post/main.py
+++ blog post/main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
+EMAIL = "ben@gmail.com"
+PASSWORD = "Be1111bd4"
 app = Flask(__name__)
 
 
@@ -24,10 +26,23 @@ def show_post(index):
 def about():
     return render_template("about.html")
 
-
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        data = request.form
+        print(data['name'])
+        print(data['email'])
+        print(data['phone'])
+        print(data['message'])
+        return "<h1>Successfully sent your message</h1>"
+    return render_template("contact.html", msg_false=False)
+
+def send_mail(name, email, phone, message):
+    email_message = f"Subject: New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(EMAIL, EMAIL, email_message)
 
 if __name__ == "__main__":
     app.run(debug=True)
