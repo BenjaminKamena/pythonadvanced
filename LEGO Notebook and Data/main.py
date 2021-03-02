59c84e9763b0535fa11597458a5b79902d48fe6d
kamena1994 <benjii1964@gmail.com>
2021-03-02 05:55:23 +0200

    lego files

---
+++ LEGO Notebook and Data/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+colors = pd.read_csv('data/colors.csv')
+
+colors.head()
+colors['name'].nunique()
+colors.groupby('is_trans').count()
--- Programming_Languages/main.py
+++ Programming_Languages/main.py
@@ -30,12 +30,15 @@ all_empty = reshaped_df.fillna(0, inplace=True)
 all_empty = reshaped_df.isna().values.any()
 #all_empty = reshaped_df.head()
 
+roll_df = reshaped_df.rolling(window=6).mean()
+
 plt.figure(figsize=(16, 10))  # make chart larger
 plt.xticks(fontsize=14)
 plt.yticks(fontsize=14)
 plt.xlabel('Date', fontsize=14)
 plt.ylabel('Number of Posts', fontsize=14)
 plt.ylim(0, 35000)
-
-plt.plot(reshaped_df.index, reshaped_df.java)
-plt.plot(reshaped_df.index, reshaped_df.python)
+for column in roll_df.columns:
+    plt.plot(roll_df.index, roll_df[column],
+             linewidth=3, label=reshaped_df[column].name)
+plt.legend(fontsize=16)
