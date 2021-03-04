d5126459d607985679288ad338f20dbd7d10b65b
kamena1994 <benjii1964@gmail.com>
2021-03-04 12:44:38 +0200

    lego data

--- LEGO Notebook and Data/main.py
+++ LEGO Notebook and Data/main.py
@@ -1,7 +1,20 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 colors = pd.read_csv('data/colors.csv')
 
 colors.head()
 colors['name'].nunique()
 colors.groupby('is_trans').count()
+
+sets = pd.read_csv('data/sets.csv')
+sets.head()
+sets.tail()
+sets.sort_values('year').head()
+sets[sets['year'] == 1949]
+
+sets.sort_values('num_parts', ascending=False).head()
+sets_by_year = sets.groupby('year').count()
+sets_by_year['set_num'].head()
+sets_by_year['set_num'].tail()
+
